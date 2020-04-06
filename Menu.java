00e4b839f587f1e8d28f217074cddaf8db48480a
cuibapn2014 <56261497+cuibapn2014@users.noreply.github.com>
2020-04-06 10:26:21 +0700

    Dự án ngon lành

---
+++ Menu.java
@@ -0,0 +1,87 @@
+package vn.edu.giadinh;
+import java.util.Scanner;
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner number = new Scanner(System.in);
+	int n;
+	double n0;
+	System.out.println(" _____________________________________________");
+	System.out.println("| Hay chon 1 trong nhung chuc nang duoi day:  |");
+	System.out.println("| 1. Giai phuong trinh bac nhat               |");
+	System.out.println("| 2. Giai phuong trinh bac hai                |");
+	System.out.println("| 3. Tinh tien dien                           |");
+	System.out.println("|                                             |");
+	System.out.println("|                                             |");
+	System.out.println("|_____________________________________________|");
+	do{System.out.print("Chon chuc nang: ");
+	n = number.nextInt();
+	}while(n < 0 || n > 3);
+	switch(n) {
+		case 1:
+			n0 = giaiPhTrinhB1();
+			System.out.println("Nghiem cua phuong trinh: x = " + n0);break;
+		case 2:
+			giaiPhTrinhB2();break;
+		case 3:
+			n0 = tinhTien();
+			System.out.println("So tien ban phai tra la: " + n0);break;
+	}	
+	}
+	
+	public static double giaiPhTrinhB1() {
+		double a, b;
+		Scanner number = new Scanner(System.in);
+		System.out.println("Chuong trinh giai phuong trinh bac 1");
+		System.out.println("Vui long nhap vao day 2 so a va b: ");
+		System.out.print("a = ");
+		a = number.nextDouble();
+		System.out.print("b = ");
+		b = number.nextDouble();
+			return -b/a;
+	}
+	
+	public static void giaiPhTrinhB2() {
+		double a, b, c, delta, n1, n2;
+		Scanner number = new Scanner(System.in);
+		System.out.println("Chuong trinh giai phuong trinh bac 2");
+		System.out.println("Vui long nhap vao day 3 so a ,b va c: ");
+		System.out.print("a = ");
+		a = number.nextDouble();
+		System.out.print("b = ");
+		b = number.nextDouble();
+		System.out.print("c = ");
+		c = number.nextDouble();
+		delta = b*b - 4*a*c;
+		if(delta >= 0) {
+			n1 = (-b + Math.sqrt(delta))/2*a;
+			n2 = (-b - Math.sqrt(delta))/2*a;
+			System.out.println("Phuong trinh co 2 nghiem phan biet:");
+			System.out.println("x1 =" + n1 + ", x2 = " + n2);
+		}else if(delta == 0) {
+			n1 = (-b - Math.sqrt(delta))/2*a;
+			System.out.println("Phuong trinh co nghiem kep: x = " + n1);
+		}else {
+			System.out.println("Phuong trinh vo nghiem");
+		}
+	}
+	
+	public static int tinhTien() {
+		int tien, soDien;
+		Scanner number = new Scanner(System.in);
+		do {
+		System.out.println("Chuong trinh Tinh tien dien:");
+		System.out.println("Hay nhap vao day so dien da tieu thu: ");
+		soDien = number.nextInt();
+		}while(soDien < 0);
+			if(soDien < 50) {
+				tien = soDien * 1000;
+			}else {
+				tien = 50 * 1000 + (soDien - 50) * 1200;
+			}
+		return tien;
+	}
+
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+echo "# my-project-nmt" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/cuibapn2014/my-project-nmt.git
+git push -u origin master
+                
